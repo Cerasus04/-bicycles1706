de44153f3f84eceaf1690cb3ef31f7acc69daed8
cerasus1004 <cerasus1004@gmail.com>
2021-06-19 16:00:53 +0300

    Подключение JS

---
+++ build/js/scripts.js
@@ -0,0 +1,26 @@
+'use strict';
+var headerNav = document.querySelector('.header__nav');
+var headerToggle = document.querySelector('.header__toggle');
+var promoBlock = document.querySelector('.promo');
+var listItems = headerNav.querySelectorAll('.header__link');
+
+headerNav.classList.remove('header__nav--nojs');
+headerNav.classList.add('header__nav--closed');
+promoBlock.classList.remove('promo--nojs');
+
+var onMenuClick = function () {
+  if (headerNav.classList.contains('header__nav--closed')) {
+    headerNav.classList.remove('header__nav--closed');
+    headerNav.classList.add('header__nav--opened');
+  } else {
+    headerNav.classList.add('header__nav--closed');
+    headerNav.classList.remove('header__nav--opened');
+  }
+};
+
+for (var i = 0; i < listItems.length; i++) {
+  listItems[i].addEventListener('click', onMenuClick);
+}
+
+headerToggle.addEventListener('click', onMenuClick);
+
